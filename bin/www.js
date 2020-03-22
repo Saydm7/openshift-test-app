887a7de763623316badc68f4e3d2de327a86c567
Saydm7 <62406521+Saydm7@users.noreply.github.com>
2020-03-21 23:52:04 -0400

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,12 @@
-var app = require('../server/server');
+/*var app = require('../server/server');
 var ip = process.env.IP || '0.0.0.0';
 var port = process.env.PORT || 8080;
 
 app.listen(port, ip, function() {
   console.log('running at ' + ip + ':' + port);
-});
+});*/
+
+
+require('http').createServer((req, res) => {
+ res.end( 'Hello World V1')
+}).listen(8080)
